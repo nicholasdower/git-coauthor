37a0972da553c5e8efbd7d88e7d5de806cc504f1
git-coauthor-ci <nicholasdower@gmail.com>
2024-01-24 08:19:19 +0000

    Add Homebrew formula for v4

--- Formula/git-coauthor.rb
+++ Formula/git-coauthor.rb
@@ -1,29 +1,28 @@
 class GitCoauthor < Formula
   desc "CLI used to manage Git coauthors"
   homepage "https://github.com/nicholasdower/git-coauthor"
-  url "https://github.com/nicholasdower/git-coauthor/archive/v3.tar.gz"
-  sha256 "50c89bbe954b716a1420c2bd7075dd9d2ef8871b708887099a4ec80fe0c1d09b"
+  url "https://github.com/nicholasdower/git-coauthor/archive/v4.tar.gz"
+  sha256 "5882b0e61143a38b33b2cf7b1e225f9020e50a98e4c240e059a5e8be997f2865"
   license "MIT"
 
   def install
     lib.install Dir["lib/*"]
     bin.install "bin/git-coauthor"
-    system 'git config --global alias.coauthor \'!git-coauthor\''
+    system "git", "config", "--global", "alias.coauthor", "'!git-coauthor'"
   end
 
   test do
-    assert_match "git-coauthor version 3", shell_output("#{bin}/git-coauthor --version")
+    assert_match "git-coauthor version 4", shell_output("#{bin}/git-coauthor --version")
   end
 
   def caveats
     <<~EOS
-      Git Coauthor installed as `git-coauthor`. Try:
+      Git Coauthor installed as `git coauthor`. Try:
 
-        git-coauthor -h
+        git coauthor -h
 
-      To add Git aliases to git run:
+      To add the ca alias:
 
-        git config --global alias.coauthor '!git-coauthor'
         git config --global alias.ca '!git-coauthor'
     EOS
   end
