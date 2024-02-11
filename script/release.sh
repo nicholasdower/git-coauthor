e83c23961e1ab6f4c710fa7c293a76d762b4f298
Nick Dower <nicholasdower@gmail.com>
2024-02-11 07:35:06 +0100

    Remove unwrap_or_else.

--- script/release.sh
+++ script/release.sh
@@ -11,40 +11,59 @@ fi
 
 version="$1"
 
+echo "Set version to $version"
 sed -i '' "s/^version = .*/version = \"$version\"/g" Cargo.toml
 
+echo "Build"
 rm -rf target
 cargo build --release --all-features
 
+echo "Lint"
+cargo clippy -- -Dwarnings
+
+echo "Test"
 RUNNER_TEMP=/tmp ./script/test.sh
 
 rm -rf bin
 mkdir -p bin
 cp ./target/release/git-coauthor bin/
 
+echo "Create man page"
 ./script/manpage.sh "$version" "$(date '+%Y-%m-%d')"
 
+echo "Create release.tar.gz"
 file="release.tar.gz"
 rm -f "$file"
 tar -czf "$file" ./man/ ./bin/
 
+echo "Create Homebrew formula"
 ./script/homebrew.sh "$version" "$file"
+
+echo "Update CHANGELOG.md"
 ./script/changelog.sh "$version"
+
+echo "Update README.md"
 ./script/readme.sh
 
+echo "Commit changes"
 git add .
-
 echo -e "v$version Release\n\n$(cat .release-notes)" | git commit -a -F -
 
+echo "Add tag v$version"
 git tag "v$version"
 
+echo "Reset .release-notes"
 cp .release-notes tmp/
 echo "- No changes" > .release-notes
 git add .release-notes
 git commit -a -m 'Post release'
 
+echo "Push changes"
 git push origin master
 git push origin "v$version"
 
+echo "Create release"
 gh release create "v$version" "$file" -R nicholasdower/git-coauthor --notes-file tmp/.release-notes
+
+echo "Trigger Homebrew update"
 gh workflow run update.yml --ref master -R nicholasdower/homebrew-tap
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use git2::{Commit, ObjectType, Repository};
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::{Path, PathBuf};
-use std::{os::unix::process::ExitStatusExt, process::Command};
+use std::{process::Command};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -66,121 +66,84 @@ struct Cli {
 }
 
 fn main() {
-    let args = Cli::try_parse().unwrap_or_else(|e| parse_error(e));
+    let output = run().unwrap_or_else(|e| {
+        eprintln!("error: {}", e);
+        std::process::exit(1);
+    });
+    if output.is_some() {
+        println!("{}", output.unwrap());
+    }
+    std::process::exit(0);
+}
+
+fn run() -> Result<Option<String>, String> {
+    let args = Cli::try_parse().map_err(|e| format!("{}\n{}", e.kind(), SHORT_HELP))?;
 
     if args.short_help {
-        short_help();
+        Ok(Some(SHORT_HELP.to_string()))
     } else if args.long_help {
-        long_help();
+        Command::new("man")
+            .arg("git-coauthor")
+            .status()
+            .map_err(|_| "failed to execute man".to_string())?;
+        return Ok(None); // todo
     } else if args.version {
-        version();
+        Ok(Some(format!("git-coauthor {}", VERSION)))
     } else if !args.aliases.is_empty() {
-        add(args.aliases);
+        let coauthors = add_to_commit(args.aliases)?.join("\n");
+        Ok(Some(coauthors))
     } else {
-        list();
+        let coauthors = read_from_commit()?;
+        if coauthors.is_empty() {
+            Ok(Some("no coauthors found".to_string()))
+        } else {
+            Ok(Some(coauthors.join("\n")))
+        }
     }
 }
 
-fn parse_error(error: clap::Error) -> ! {
-    eprintln!("error: {}", error.kind());
-    eprintln!("{}", SHORT_HELP);
-    std::process::exit(1);
-}
-
-fn error(message: &str) -> ! {
-    eprintln!("error: {}", message);
-    std::process::exit(1);
-}
-
-fn short_help() {
-    print!("{}", SHORT_HELP);
-    std::process::exit(0);
-}
-
-fn long_help() {
-    let status = Command::new("man")
-        .arg("git-coauthor")
-        .status()
-        .unwrap_or_else(|_| error("failed to execute man"));
-    std::process::exit(status.into_raw());
-}
-
-fn version() {
-    println!("git-coauthor {}", VERSION);
-    std::process::exit(0);
-}
-
-fn add(aliases: Vec<String>) {
-    let config = get_config();
-    let coauthors: Vec<String> = aliases.iter().map(|alias| {
-        let coauthor = config.get(alias);
-        let mut line = coauthor.unwrap_or_else(|| error("coauthor not found")).to_string();
-        line = format!("Co-authored-by: {}", line);
-        line
-    }).collect();
-    let new_coauthors = add_to_commit(coauthors);
-    new_coauthors.iter().for_each(|line| println!("{}", line));
-}
-
-fn list() {
-    let repo = Repository::open_from_env().unwrap_or_else(|_| error("failed to find repository"));
-    let commit = get_commit(&repo).unwrap_or_else(|_| error("failed to get commit"));
-    let message = commit
-        .message()
-        .unwrap_or_else(|| error("failed to get commit message"));
-    let coauthors: Vec<&str> = message
-        .lines()
-        .filter(|line| line.starts_with("Co-authored-by:"))
-        .collect();
-    if coauthors.is_empty() {
-        println!("no coauthors found");
-    }
-    coauthors.iter().for_each(|&line| println!("{}", line));
-    std::process::exit(0);
-}
-
-fn get_config_for(path: &Path) -> HashMap<String, String> {
+fn get_config_for(path: &Path) -> Result<HashMap<String, String>, String> {
     let file = File::open(path);
     if file.is_err() {
-        return HashMap::new();
+        return Ok(HashMap::new());
     }
     let config: Result<HashMap<String, String>, serde_yaml::Error> =
         serde_yaml::from_reader(file.unwrap());
-    config.unwrap_or_else(|_| error("failed to read configuration"))
+    config.map_err(|_| "failed to read configuration".to_string())
 }
 
-fn get_user_config() -> HashMap<String, String> {
+fn get_user_config() -> Result<HashMap<String, String>, String> {
     let home_dir: Option<PathBuf> = home::home_dir();
     if home_dir.is_some() {
         let mut user_path_buf = home_dir.unwrap();
         user_path_buf.push(Path::new(".gitcoauthors"));
         return get_config_for(user_path_buf.as_path());
     } else {
-        HashMap::new()
+        Ok(HashMap::new())
     }
 }
 
-fn get_repo_config() -> HashMap<String, String> {
-    let repo = Repository::open_from_env().unwrap_or_else(|_| error("failed to find repository"));
+fn get_repo_config() -> Result<HashMap<String, String>, String> {
+    let repo = Repository::open_from_env().map_err(|_| "failed to find repository".to_string())?;
     let mut path_buf = repo.path().to_path_buf();
     path_buf.push("..");
     path_buf.push(".gitcoauthors");
     return get_config_for(path_buf.as_path());
 }
 
-fn get_repo_git_config() -> HashMap<String, String> {
-    let repo = Repository::open_from_env().unwrap_or_else(|_| error("failed to find repository"));
+fn get_repo_git_config() -> Result<HashMap<String, String>, String> {
+    let repo = Repository::open_from_env().map_err(|_| "failed to find repository".to_string())?;
     let mut path_buf = repo.path().to_path_buf();
     path_buf.push("coauthors");
     return get_config_for(path_buf.as_path());
 }
 
-fn get_config() -> HashMap<String, String> {
+fn get_config() -> Result<HashMap<String, String>, String> {
     let mut config = HashMap::new();
-    config.extend(get_user_config());
-    config.extend(get_repo_git_config());
-    config.extend(get_repo_config());
-    config
+    config.extend(get_user_config()?);
+    config.extend(get_repo_git_config()?);
+    config.extend(get_repo_config()?);
+    Ok(config)
 }
 
 fn get_commit(repo: &Repository) -> Result<Commit, git2::Error> {
@@ -189,13 +152,40 @@ fn get_commit(repo: &Repository) -> Result<Commit, git2::Error> {
         .map_err(|_| git2::Error::from_str("error: couldn't find commit"))
 }
 
-fn add_to_commit(coauthors: Vec<String>) -> Vec<String> {
-    let repo = Repository::open_from_env().unwrap_or_else(|_| error("failed to find repository"));
-    let head = repo.head().unwrap_or_else(|_| error("failed to find head"));
-    let commit = head.peel_to_commit().unwrap_or_else(|_| error("failed to find commit"));
-    let tree = commit.tree().unwrap_or_else(|_| error("failed to find tree"));
+fn read_from_commit() -> Result<Vec<String>, String> {
+    let repo = Repository::open_from_env().map_err(|_| "failed to find repository".to_string())?;
+    let commit = get_commit(&repo).map_err(|_| "failed to get commit".to_string())?;
+    if commit.message().is_none() {
+        return Err("failed to read commit message".to_string());
+    }
+    let message = commit.message().unwrap();
+    let coauthors: Vec<String> = message
+        .lines()
+        .filter(|line| line.starts_with("Co-authored-by:"))
+        .map(|line| line.to_string())
+        .collect();
+    Ok(coauthors)
+}
 
-    let message = commit.message().unwrap_or_else(|| error("failed to read commit message")).to_string();
+fn add_to_commit(aliases: Vec<String>) -> Result<Vec<String>, String> {
+    let config = get_config()?;
+    let contains_all_keys = aliases.iter().all(|key| config.contains_key(key));
+    if !contains_all_keys {
+        return Err("coauthor not found".to_string());
+    }
+    let coauthors: Vec<String> = aliases.iter().map(|alias| {
+        let coauthor = config.get(alias);
+        format!("Co-authored-by: {}", coauthor.unwrap())
+    }).collect();
+    let repo = Repository::open_from_env().map_err(|_| "failed to find repository".to_string())?;
+    let head = repo.head().map_err(|_| "failed to find head".to_string())?;
+    let commit = head.peel_to_commit().map_err(|_| "failed to find commit".to_string())?;
+    let tree = commit.tree().map_err(|_| "failed to find tree".to_string())?;
+
+    if commit.message().is_none() {
+        return Err("failed to read commit message".to_string());
+    }
+    let message = commit.message().unwrap();
     let mut lines: Vec<String> = message.lines().map(|line| line.to_string()).collect();
     let mut existing: Vec<String> = lines
         .iter()
@@ -221,7 +211,6 @@ fn add_to_commit(coauthors: Vec<String>) -> Vec<String> {
         None,
         Some(new_message.as_str()),
         Some(&tree),
-    ).unwrap_or_else(|_| error("failed to amend commit"));
-
-    existing
+    ).map_err(|_| "failed to amend commit".to_string())?;
+    Ok(existing)
 }
