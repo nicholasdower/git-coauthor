3f782b2c7afc8134f6c5b36a63530e8e186f6cf2
Nick Dower <nicholasdower@gmail.com>
2024-02-12 18:27:01 +0100

    Update test style.

--- script/test.sh
+++ script/test.sh
@@ -1,117 +1,110 @@
 #!/usr/bin/env bash
 
 if [[ -z "$RUNNER_TEMP" ]]; then
-  dir="/tmp"
+  dir="/tmp/git-coauthor"
 else
-  dir="$RUNNER_TEMP"
+  dir="$RUNNER_TEMP/git-coauthor"
 fi
 
+rm -rf "$dir"
+mkdir -p "$dir"
 cp ./target/release/git-coauthor "$dir"
 cd "$dir"
-rm -rf sample-repo
-mkdir -p sample-repo
-mv ./git-coauthor sample-repo
-cd sample-repo
 
 function test() {
   name="$1"
-  actual="$2"
-  expected="$3"
-  if [ "$actual" = "$expected" ]; then
+  if diff expected actual > /dev/null; then
     printf "\033[0;32m"
     echo "test passed: $name"
     printf "\033[0m"
   else
     printf "\033[0;31m"
     echo "test failed: $name"
-    echo "expected:"
-    echo "$expected"
-    echo "actual:"
-    echo "$actual"
     printf "\033[0m"
+    diff expected actual
     exit 1
   fi
 }
 
-actual=$(./git-coauthor 2>&1)
-expected=$(echo "error: failed to find repository")
-test "no repository" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'error: failed to find repository\n' > expected
+test "no repository"
 
 touch foo
 git init --initial-branch=master --quiet
 git config user.email "nicholasdower@gmail.com"
 git config user.name "git-coauthor-ci"
 
-actual=$(./git-coauthor 2>&1)
-expected=$(echo "error: failed to find head")
-test "no commit" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'error: failed to find head\n' > expected
+test "no commit"
 
 git add foo
 git commit -m 'foo' --quiet
 
 echo 'foo: bar' > .git/coauthors
 
-actual=$(./git-coauthor foo 2>&1)
-expected=$(echo "error: failed to read configuration")
-test "invalid config" "$actual" "$expected"
+./git-coauthor foo > actual 2>&1
+printf 'error: failed to read configuration\n' > expected
+test "invalid config"
 
 echo 'foo = "Foo <foo@foo.com>"' > .git/coauthors
 echo 'bar = "Bar <bar@bar.com>"' >> .git/coauthors
 
-actual=$(./git-coauthor 2>&1)
-expected=$(echo "no coauthors")
-test "list no coauthors" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'no coauthors\n' > expected
+test "list no coauthors"
 
-actual=$(./git-coauthor baz 2>&1)
-expected=$(echo "error: coauthor not found")
-test "bad coauthor" "$actual" "$expected"
+./git-coauthor baz > actual 2>&1
+printf 'error: coauthor not found\n' > expected
+test "bad coauthors"
 
-actual=$(./git-coauthor foo 2>&1)
-expected=$(echo "Co-authored-by: Foo <foo@foo.com>")
-test "add one coauthor" "$actual" "$expected"
+./git-coauthor foo > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\n' > expected
+test "add one coauthor"
 
-actual=$(./git-coauthor 2>&1)
-expected=$(echo "Co-authored-by: Foo <foo@foo.com>")
-test "list one coauthor" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\n' > expected
+test "list one coauthor"
 
-actual=$(./git-coauthor bar 2>&1)
-expected=$(printf "Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "add another coauthor" "$actual" "$expected"
+./git-coauthor bar > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "list one coauthor"
 
-actual=$(./git-coauthor -d foo 2>&1)
-expected=$(echo "Co-authored-by: Bar <bar@bar.com>")
-test "remove one coauthor" "$actual" "$expected"
+./git-coauthor -d foo > actual 2>&1
+printf 'Co-authored-by: Bar <bar@bar.com>\n' > expected
+test "remove one coauthor"
 
 git commit --amend -m 'foo' --quiet
 
-actual=$(./git-coauthor foo bar 2>&1)
-expected=$(printf "Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "add multiple coauthors" "$actual" "$expected"
+./git-coauthor foo bar > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "add multiple coauthors"
 
-actual=$(./git-coauthor 2>&1)
-expected=$(printf "Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "list multiple coauthors" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "list multiple coauthors"
 
-actual=$(./git-coauthor foo 2>&1)
-expected=$(printf "Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "add same coauthor" "$actual" "$expected"
+./git-coauthor foo > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "add same coauthor"
 
-actual=$(./git-coauthor 2>&1)
-expected=$(printf "Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "list multiple coauthors again" "$actual" "$expected"
+./git-coauthor > actual 2>&1
+printf 'Co-authored-by: Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "list multiple coauthors again"
 
-actual=$(./git-coauthor -d foo bar 2>&1)
-expected=$(echo "no coauthors")
-test "delete multiple coauthors" "$actual" "$expected"
+./git-coauthor -d foo bar > actual 2>&1
+printf 'no coauthors\n' > expected
+test "delete multiple coauthors"
 
 git commit --amend -m 'foo' --quiet
 
 echo 'foo = "Other Foo <foo@foo.com>"' > .gitcoauthors
 
-actual=$(./git-coauthor foo bar 2>&1)
-expected=$(printf "Co-authored-by: Other Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n")
-test "override coauthor" "$actual" "$expected"
+./git-coauthor foo bar > actual 2>&1
+printf 'Co-authored-by: Other Foo <foo@foo.com>\nCo-authored-by: Bar <bar@bar.com>\n' > expected
+test "override coauthor"
 
-actual=$(./git-coauthor -d 2>&1)
-expected=$(echo "no coauthors")
-test "delete all coauthors" "$actual" "$expected"
+./git-coauthor -d > actual 2>&1
+printf 'no coauthors\n' > expected
+test "delete all coauthors"
